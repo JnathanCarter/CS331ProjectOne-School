dbacc86b40e050200504fc5d9fc2eb8529c67ae0
JnathanCarter <jcarter20@stu.jsu.edu>
2022-09-11 20:41:42 -0500

    default student class

--- School/src/school/Student.java
+++ School/src/school/Student.java
@@ -10,5 +10,23 @@ package school;
  * @author johnc
  */
 public class Student {
-    
+        private String firstName, lastName;
+
+        private Address homeAddress, schoolAddress;
+
+        public Student(String first, String last, Address home, Address school) {
+                this.firstName = first;
+                this.lastName = last;
+                this.homeAddress = home;
+                this.schoolAddress = school;
+        }
+
+        public String toString
+        {
+                String result;
+                result = this.firstName + " " + this.lastName + "\n";
+                result += "Home Address: \n" + homeAddress + "\n";
+                result += "School address: \n" + schoolAddress;
+                return result;
+        }
 }
