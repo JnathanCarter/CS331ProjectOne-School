caf7cbadb89a929ad4f56971cae96db8153bfa1b
JnathanCarter <jcarter20@stu.jsu.edu>
2022-09-11 22:30:00 -0500

    added initial print out for main and initializ students

--- School/src/school/main.java
+++ School/src/school/main.java
@@ -15,7 +15,17 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Address homeAddress1 = new Address("452 Church ave", "Anniston", "AL", 36207);
+        Address schoolAddress1 = new Address("1600 Pelham Rd", "Jacksonville", "AL", 36265);
+
+        Address homeAddress2 = new Address("507 Snow Street", "Oxford", "AL", 36203);
+        Address schoolAddress2 = new Address("1600 Pelham Rd", "Jacksonville", "AL", 36265);
+
+        Student s1 = new Student("Jonathan", "Carter", homeAddress1, schoolAddress1);
+        Student s2 = new Student("Maurzel", "Castleberry", homeAddress2, schoolAddress2, 12, 80, 88);
+
+        System.out.println("Initial Value-----------\n" + s1.toString() + "\n" + s2.toString());
+
     }
 
 }
